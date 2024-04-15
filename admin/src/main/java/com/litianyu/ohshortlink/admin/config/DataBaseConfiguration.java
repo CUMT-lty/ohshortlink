4e50e16ceecc73d82164848668d48bccb509478e
CUMT-lty <m13182371876@163.com>
2024-04-15 21:29:51 +0800

    admin模块、project模块：添加分页配置

---
+++ admin/src/main/java/com/litianyu/ohshortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,26 @@
+package com.litianyu.ohshortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 数据库持久层配置类
+ */
+@Configuration // (value = "dataBaseConfigurationByAdmin")
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptorByAdmin() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ project/src/main/java/com/litianyu/ohshortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.litianyu.ohshortlink.project.config;
+
+
+/**
+ * 数据库持久层配置类
+ */
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() { // mybatis plus 分页插件
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
