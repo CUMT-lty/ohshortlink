218980b97ad89a4ff2097ea43856b0f91a0ceb21
CUMT-lty <m13182371876@163.com>
2024-07-12 00:06:02 +0800

    fix：修复短链接访问记录中的访客类型字段展示错误

--- project/src/main/java/com/litianyu/ohshortlink/project/service/impl/ShortLinkStatsServiceImpl.java
+++ project/src/main/java/com/litianyu/ohshortlink/project/service/impl/ShortLinkStatsServiceImpl.java
@@ -256,7 +256,7 @@ public class ShortLinkStatsServiceImpl implements ShortLinkStatsService {
             String uvType = uvTypeList.stream()
                     .filter(item -> Objects.equals(each.getUser(), item.get("user")))
                     .findFirst()
-                    .map(item -> item.get("UvType"))
+                    .map(item -> item.get("uvType"))
                     .map(Object::toString)
                     .orElse("旧访客");
             each.setUvType(uvType);
@@ -449,7 +449,7 @@ public class ShortLinkStatsServiceImpl implements ShortLinkStatsService {
             String uvType = uvTypeList.stream()
                     .filter(item -> Objects.equals(each.getUser(), item.get("user")))
                     .findFirst()
-                    .map(item -> item.get("UvType"))
+                    .map(item -> item.get("uvType"))
                     .map(Object::toString)
                     .orElse("旧访客");
             each.setUvType(uvType);
