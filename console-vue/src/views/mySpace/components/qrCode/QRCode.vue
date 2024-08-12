7c3a5ac168db6b4556dff2b2802a485d0e22001b
CUMT-lty <m13182371876@163.com>
2024-08-12 21:52:47 +0800

    feature：前端

---
+++ console-vue/src/views/mySpace/components/qrCode/QRCode.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted, nextTick } from 'vue'
+import { ElMessage } from 'element-plus'
+import QRCode from 'qrcode'
+</script>
+
+<style lang="scss" scoped>
+.main-box {
+  display: flex;
+  flex-direction: column;
+}
+
+.qr-code {
+  cursor: pointer !important;
+}
+
+.qr-code:hover {
+  opacity: .5;
+}
+</style>
